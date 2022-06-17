438d2d54efbded10732d7d7dfcd28973fcc3bfd1
Shelovescolin <colin.ledru@gmail.com>
2022-06-17 11:46:42 +0200

    pour breval

--- everything.py
+++ everything.py
@@ -24,11 +24,12 @@ print('\n'*(m//2-1))
 
 for packet in capture:
     if hasattr(packet,'sip'):
+        # print(packet.sip._all_fields)
         desc = list(packet.sip._all_fields.values())[0].replace('SIP/2.0 ','').replace('SIP/2.0','')
         desc = (' '*m+('{:->'+str(sizex-m*2)+'}' if desc[0].isdigit() else '{}')).format(desc)
         print(desc)
         if hasattr(packet.sip,'sip.Method'):
-
+            
             #récupération des IPs dans le paquet INVITE
             if packet.sip.Method == 'INVITE':
                 if not IPs:
@@ -48,6 +49,13 @@ for packet in capture:
                 CID = packet.sip.call_id_generated
                 print('\n'*(m//2)+'Call ended.\nCID '+CID)
                 break
+        elif hasattr(packet.sip,'sip.Status-Code'):
+            print(packet.sip._all_fields)
+            if packet.sip._all_fields['sip.Status-Code'] == '200':
+                start_call_time = packet.sip.Date
+                print(f"\n\n\n\n\nWWWWWIIIIINNNNNN\n\n\n\n\n\n\n\n\n\ {start_call_time}")
+        else:
+            print("No status code")
     else:
         if not started:
             started = True
@@ -92,3 +100,10 @@ for rtp in RTPs2:
 system(f'sox --type raw --rate {fs} -e u-law {IPs[0]}.g711u {IPs[0]}.wav')
 system(f'sox --type raw --rate {fs} -e u-law {IPs[1]}.g711u {IPs[1]}.wav')
 system(f'sox -M {IPs[0]}.wav {IPs[1]}.wav {CID}.wav')
+
+CID = f"ID du call : {CID}\n"
+
+call_infos = open('infos_call.txt', 'w')
+for id in CID:
+    call_infos.write(id)
+
